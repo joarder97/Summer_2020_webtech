232f4a30738484e5425e327392f95d1f18b6cc47
joarder97 <masudul.pallab99@gmail,com>
2020-07-29 23:57:42 +0600

    regcheckAdded

--- PHP_SESSION_TASK_04/regcheck.php
+++ PHP_SESSION_TASK_04/regcheck.php
@@ -2,15 +2,20 @@
 	
 	session_start();
 	
-	if(isset($_POST['submit'])){
+	if($_SERVER["REQUEST_METHOD"] == "POST")
+	{
 
-		$uname 		= $_POST['name'];
+		$name 		= $_POST['name'];
 		$email 		= $_POST['email'];
 		$userName   = $_POST['userName'];
 		$password   = $_POST['password'];
 		$confirmPassword   = $_POST['confirmPassword'];
+		$gender   = $_POST['gender'];
+		$date   = $_POST['date'];
+		$month   = $_POST['month'];
+		$year   = $_POST['year'];
 
-		if(empty($name) || empty($email) || empty($userName) || empty($password) || empty($confirmPassword) ){
+		if(empty($name) || empty($email) || empty($userName) || empty($password) || empty($confirmPassword) || empty($gender) || empty($day) || empty($month) || empty($year)  ){
 			echo "null submission found!";
 		}else{
 			$_SESSION['name'] 		= $name;
@@ -18,13 +23,21 @@
 			$_SESSION['userName'] 		= $userName;
 			$_SESSION['password'] 		= $password;
 			$_SESSION['confirmPassword'] 		= $confirmPassword;
+			$_SESSION['gender'] = $gender;
+			$_SESSION['day'] 		= $day;
+ 			$_SESSION['month'] 		= $month;
+ 			$_SESSION['year'] 		= $year;
 
 			echo "done!";
 		}	
 
+
 	}
 
-
+else
+{
+	echo "invalid request";
+}
 
 
 ?>
