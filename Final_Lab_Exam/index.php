8b0bae5c3dde7ea5cdb3c35faf250e8b5abf8e4b
joarder97 <masudul.pallab99@gmail,com>
2020-09-07 11:38:47 +0600

    finalLabExam

---
+++ Final_Lab_Exam/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+</head>
+<body>
+	<h1>Landing Page</h1>
+	<a href="views/signup.php">Admin Sign Up</a> || <a href="views/login.php">Admin Login</a> || <a href="views/empLogin.php">Employee Login</a>
+</body>
+</html>
